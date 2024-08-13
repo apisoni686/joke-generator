150cdd4a26c07469c735cce9faca9ab0b9f05429
Amy :) <apisoni686@gmail.com>
2024-08-12 18:58:07 -0700

    fixed functions

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-new Typewriter(``, {
-  strings: "",
-  autoStart: true,
-});
-
-function tellJoke(response) {
+function displayJoke(response) {
+  console.log;
   let jokeElement = document.querySelector("#joke");
-  jokeElement.innerHTML = response.data;
+  jokeElement.innerHTML = response.data.answer;
+
+  new Typewriter("#joke", {
+    strings: response.data.answer,
+    autoStart: true,
+  });
+}
+
+function tellJoke(event) {
+  event.preventDefault();
+
+  let key = "df01d4d69fab3f5otaf6694bc9e08ea8";
+  let prompt = "Tell me a random joke about millenials.";
+  let context =
+    "You are an expert AI assistant on the millenial generation. Please don't make the same joke every time.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  axios.get(apiUrl).then(displayJoke);
 }
 
 let jokeButton = document.querySelector("button");
 jokeButton.addEventListener("click", tellJoke);
-
-let key = "df01d4d69fab3f5otaf6694bc9e08ea8";
-
-let prompt = "Tell me a joke about millenial adults.";
-let context =
-  "You are an expert AI assistant on the millenial generation. But you are not mean.";
-
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
-// axios.get(apiUrl);
-console.log(apiUrl);
